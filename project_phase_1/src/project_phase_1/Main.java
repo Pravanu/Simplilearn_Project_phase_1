685c085753ad1c8b90852b503c71cdd2dfc8f709
VANU <VANU@DESKTOP-VUG3GVB>
2022-05-01 11:42:44 +0530

    First Commit

---
+++ project_phase_1/src/project_phase_1/Main.java
@@ -0,0 +1,13 @@
+package project_phase_1;
+
+public class Main {
+
+public static final String path = "F:\\SimplilearnProject\\phs_1"; 
+	
+	public static void main(String[] args) {
+		Menus menu = new Menus();
+		menu.introScreen();
+		menu.mainMenu();
+	}
+	
+}
